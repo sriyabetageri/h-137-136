1b74bd9a1df6a6e70b313f6c8df471693775e360
sriyabetageri <75461862+sriyabetageri@users.noreply.github.com>
2022-03-21 18:59:03 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import{createAppContainer} from "react-navigation";
+import{createStackNavigator} from "react-navigation-stack";
+import HomeScreen from "./screens/Home";
+import DetailsScreen from "./screens/Details";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return <AppContainer/>
+}
+const appStackNavigator = createStackNavigator(
+  {
+    Home:{
+      screen:HomeScreen,
+      navigationOptions:
+      {
+        headerShown: false
+      }
+    },
+    Details:{
+      screen:DetailsScreen,
+    }
+  },
+  {
+    initialRouteName:"Home"
+  }
+);
+
+
+const AppContainer = createAppContainer(appStackNavigator)
+
